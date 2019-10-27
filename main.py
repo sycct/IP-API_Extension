6a00f195e73be66ce7f3aab007d9014b71afe04e
sycct <huang_9119596@163.com>
2019-10-27 10:15:20 +0800

    [1.0.3.WIP] 2019-10-24 add headers and request methods.

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/get_ip_info/<string:ip>')
+@app.route('/get_ip_info/<string:ip>', methods=['GET'])
 def get_ip_info(ip):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
     try:
-        request_ip = requests.get('http://ip-api.com/json/{}?fields=16515071&lang=zh-CN'.format(ip))
+        request_ip = requests.get('http://ip-api.com/json/{}?fields=16515071&lang=zh-CN'.format(ip), headers=headers)
         response_text = request_ip.json()
-    except:
+    except requests.exceptions.RequestException:
         response_text = {'status': 'error'}
 
     return jsonify(response_text)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
